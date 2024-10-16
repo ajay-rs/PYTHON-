f7cc99b0136373f5a949c8dfddedcab25e5578cb
ajay-rs <ajayrav516@gmail.com>
2024-10-16 09:54:09 +0530

    python

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,35 @@
+#Left side:keys ,Right side:value pair
+d = {"chicken":110, "mutton":150, "egg":100, "plain":100}
+print(d)
+
+
+
+
+
+
+#Name(key) and rollno(valuepair) 
+students = {}
+count = int(input("Enter no. of students: "))
+for i in range(count):
+    name = input("Enter Name: ")
+    roll_no = int(input("Enter Roll no. "))
+    students[name] = roll_no
+
+print(students)
+print(students.keys())
+print(students.values())
+
+
+
+
+#
+fruits = {"banana":100, "apple":200, "orange":220}
+total = 0
+for fruit_price in fruits.values(): 
+    total = total + fruit_price
+
+print(total)
+
 
 
 #
@@ -10,3 +42,10 @@ for fruit_price in fruits.values():
 print(total)
 
 
+fruits = {"banana":100, "apple":200, "orange":220}
+total = 0
+for fruit_price in fruits.values(): 
+    fruit_name = input("What fruit you bought: ")
+    total = total + fruits.get(fruit_name)
+
+print(total)
---
+++ interview.py
@@ -0,0 +1,17 @@
+l1 = ['p','t','o']
+l2 = ['y','h','n']
+#python
+for no in range(len(l1)):
+    print(l1[no]+l2[no],end='')
+
+
+
+
+
+
+l1 = ['l','b','n']
+l2 = ['i','i']
+small = len(l1) if len(l1)<len(l2) else len(l2)
+#python
+for no in range(small):
+    print(l1[no]+l2[no],end='')
