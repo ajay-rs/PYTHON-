365f913df40778235bece5c0f0aed1a6ed2ba715
ajay-rs <ajayrav516@gmail.com>
2024-10-20 13:26:18 +0530

    Python

---
+++ oops.py
@@ -0,0 +1,16 @@
+class Players:
+ 
+
+    def bat(self):
+        print("Batting")
+    
+    def field(self):
+        print("Fielding")
+
+    def bowl(self):
+        print("Bowling")
+
+dhoni = Players()
+dhoni.bat()
+dhoni.field()
+dhoni.bowl()
